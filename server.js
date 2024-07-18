9343ecafad7c972d9a4ebe04f2abf014d209fbc4
Shayan <sramezanzadeh1@myseneca.ca>
2024-07-18 16:49:28 -0400

    AS4

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ app.get('/categories', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
 });
